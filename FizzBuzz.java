a87cc2b9da226ed3a288e5caad2b4979c434a82c
ciro000 <maqqimaqui@gmail.com>
2024-03-20 21:22:03 -0300

    subiendo ejercicio fizzbuzz

---
+++ FizzBuzz.java
@@ -0,0 +1,25 @@
+public class FizzBuzz {
+    public static String fizzBuzz(int number){
+
+        
+        if(number%5 == 0 & number%3 == 0){
+            return("FizzBuzz");
+        }
+
+        else if(number%5 == 0){
+            return("Buzz");
+        }
+
+        else if(number%3 == 0){
+            return("Fizz");
+        }
+
+        return Integer.toString(number);
+    }
+
+    public static void main (String [] args){
+
+        System.out.println(fizzBuzz(2));
+    }
+
+}
